f720f609b168e980da019e24a9e709b949ddf8f1
Oleg <slobodchikov_1990@mail.ru>
2023-03-24 19:11:01 +0300

    solved task 25

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int a;
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out a);
+int Znach (int a){
+    int sum = 0;
+    for (int i = a; i>0; i--)
+    {   sum = sum + a%10;
+        a/=10; 
+    }
+    return sum;
+}
+
+Console.WriteLine($"Итоговое число {Znach(a)}");
